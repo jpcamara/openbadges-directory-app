04ba19694fd1be845e9c3e33239c0009fb4e63aa
jpcamara <johnpcamara@gmail.com>
2014-06-19 11:27:13 -0400

    fixing up the code to be dynamically configurable

---
+++ README.md
@@ -0,0 +1,9 @@
+Directory App
+---
+
+Example application interfacing with the directory api.
+
+## Env Vars
+
+    ENDPOINT     for instance, http://directory.openbadges.org
+    API_KEY      the key you were issued, for instance 1234567
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,34 +1,37 @@
 'use strict';
 
-var directoryControllers = angular.module('directoryControllers', []),
+var directoryControllers = angular.module('directoryControllers', ['base64']),
     validUrl = function (url) {
 //      return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
       return true;
     };
 
-directoryControllers.controller('DirectoryController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-//   $http.defaults.headers.common = {"Access-Control-Request-Headers": "accept, origin, authorization"};
-   $http.defaults.headers.common = {'Authorization': 'Basic MTIzNDU2Nw=='};
+directoryControllers.controller('DirectoryController', ['$scope', '$http', '$location', '$window', '$base64',
+  function ($scope, $http, $location, $window, $base64) {
+    var apiKey = $window.config.apiKey;
 
-  $scope.searchOn = function (placeholder) {
-    $scope.search = '';
-    $scope.placeholder = placeholder;
-    $scope.disabled = false;
-  };
-  $scope.searchOff = function (placeholder) {
-    $scope.search = '';
-    $scope.placeholder = placeholder;
-    $scope.disabled = true;
-  };
-  $scope.isActive = function (path) {
-    return $location.path().substr(0, path.length) == path;
-  };
-  $scope.addTagFromForm = function () {
-    $scope.tagEnter = $scope.tagEnter == null ? true : !$scope.tagEnter;
-  };
-}]);
+    $http.defaults.headers.common = {'Authorization': 'Basic ' + $base64.encode(apiKey)};
 
-directoryControllers.controller('RecentController', ['$scope', '$http', function ($scope, $http) {
+    $scope.searchOn = function (placeholder) {
+      $scope.search = '';
+      $scope.placeholder = placeholder;
+      $scope.disabled = false;
+    };
+    $scope.searchOff = function (placeholder) {
+      $scope.search = '';
+      $scope.placeholder = placeholder;
+      $scope.disabled = true;
+    };
+    $scope.isActive = function (path) {
+      return $location.path().substr(0, path.length) == path;
+    };
+    $scope.addTagFromForm = function () {
+      $scope.tagEnter = $scope.tagEnter == null ? true : !$scope.tagEnter;
+    };
+  }]);
+
+directoryControllers.controller('RecentController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+  var endpoint = $window.config.endpoint;
   $scope.$parent.searchOff('Search not supported for Recent items');
 
   var page = 0,
@@ -39,7 +42,7 @@ directoryControllers.controller('RecentController', ['$scope', '$http', function
     if (page > 10) {
       done = true;
     }
-    $http.get('/recent?page=' + page).success(function (response) {
+    $http.get(endpoint + '/recent?page=' + page).success(function (response) {
       if (!response.data.length) { done = true; }
       if ($scope.badges && $scope.badges.length) {
         response.data.forEach(function (item) { $scope.badges.push(item); });
@@ -50,17 +53,19 @@ directoryControllers.controller('RecentController', ['$scope', '$http', function
   };
 }]);
 
-directoryControllers.controller('GetController', ['$scope', '$http', function ($scope, $http) {
+directoryControllers.controller('GetController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+  var endpoint = $window.config.endpoint;
   $scope.$parent.searchOn('Please enter a badge class url');
   $scope.$watch('search', function () {
     if (!validUrl($scope.search)) { return; }
-    $http.get('/' + encodeURIComponent($scope.search)).success(function (response) {
+    $http.get(endpoint + '/' + encodeURIComponent($scope.search)).success(function (response) {
       $scope.badges = [response.data];
     });
   });
 }]);
 
-directoryControllers.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
+directoryControllers.controller('SearchController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+  var endpoint = $window.config.endpoint;
   $scope.$parent.searchOn('What are searching for?');
   $scope.tag = '';
   $scope.tags = [];
@@ -86,7 +91,7 @@ directoryControllers.controller('SearchController', ['$scope', '$http', function
     if ($scope.search) { query.push('q=' + encodeURIComponent($scope.search)); }
     if ($scope.tags.length) { query.push('tags=' + $scope.tags.join(',')); }
 
-    $http.get('/search?page=' + page + (query.length ? '&' + query.join('&') : '')).success(function (response) {
+    $http.get(endpoint + '/search?page=' + page + (query.length ? '&' + query.join('&') : '')).success(function (response) {
       if (!response.data.length) { done = true; }
       if ($scope.badges && $scope.badges.length) {
         response.data.forEach(function (item) { $scope.badges.push(item); });
